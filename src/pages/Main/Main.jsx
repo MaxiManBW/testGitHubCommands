ecd97901f3473cb1a492f0dbe09fe2f5ab11f168
Maksym Karamanov <maximanbw@gmail.com>
2021-03-19 19:14:02 +0200

    Add main page from test

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,13 @@
+import Header from '../../components/Header';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+
+export const Main = () => {
+    return (
+        <>
+            <Header />
+            <Navbar />
+            <Footer />
+        </>
+    )
+}
